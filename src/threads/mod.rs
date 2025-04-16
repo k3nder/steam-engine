b730e28dc06a72007c6a0a33b252c559b59f49da
k3nder <k3nde@outlook.es>
2025-04-16 14:02:32 +0200

    optimize patrons

--- src/threads/mod.rs
+++ src/threads/mod.rs
@@ -1,13 +1,13 @@
 pub mod channel;
 
 macro_rules! thread {
-    ($id:expr, $communicator:ident, $body:ident) => {{
+    ($body:block) => {{ std::thread::spawn(move || $body) }};
+    ($id:expr, $communicator:expr, $body:expr) => {{
         let comm = $communicator.channel($id);
         std::thread::spawn(move || $body(comm))
     }};
-    ($id:expr, $communicator:expr, $body:ident, $args:expr) => {{
+    ($id:expr, $communicator:expr, $body:block, $args:expr) => {{
         let comm = $communicator.channel($id);
         std::thread::spawn(move || $body(comm, $args))
     }};
-    ($body:block) => {{ std::thread::spawn(move || $body) }};
 }
